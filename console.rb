24b1ed5e37e6985dcff2c506b8cff571858f0e7c
Alina <lindenalina.al@gmail.com>
2020-07-01 14:48:36 +0100

    create and save album passed

--- console.rb
+++ console.rb
@@ -1,6 +1,8 @@
 require('pry-byebug')
 require_relative('models/artist')
+require_relative('models/album')
 
+Album.delete_all()
 Artist.delete_all()
 
 artist1 = Artist.new( {
@@ -8,6 +10,14 @@ artist1 = Artist.new( {
     'last_name' => 'Grohl'
 })
 artist1.save()
+
+album1 = Album.new( {
+    'title' => 'One by One',
+    'genre' => 'Alternative rock',
+    'artist_id' => artist1.id
+})
+
+album1.save()
 
 binding.pry
 nil
